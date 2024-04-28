1bda02c788e8cb263f9ff2beb373845f2e6e4a52
Puneet Talwar <puneeet@amazon.com>
2024-04-28 22:00:27 +1000

    Going for a push model

--- spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
+++ spring-boot-hello-world-example/src/main/java/com/helloworld/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ public class HelloWorldController
 @RequestMapping("/")
 public String hello() 
 {
-return "<h1> Finally Case insensitive bug fixed!!! </h1>";
+return "<h1> Going for a push model!!! </h1>";
 }
 }
